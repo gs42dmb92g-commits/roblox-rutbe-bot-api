e8a9e41ebb2526af69c1ef970a21973630acffdc
gs42dmb92g-commits <gs42dmb92g@privaterelay.appleid.com>
2026-07-10 16:02:04 +0300

    Update rutbe.js

--- api/rutbe.js
+++ api/rutbe.js
@@ -1,18 +1,8 @@
 export default async function handler(req, res) {
-
   try {
 
-    const { userId } = req.body;
-
-    if (!userId) {
-      return res.status(400).json({
-        hata: "userId gerekli"
-      });
-    }
-
-
     const response = await fetch(
-      `https://apis.roblox.com/cloud/v2/groups/${process.env.ROBLOX_GROUP_ID}/memberships?filter=user.id%3D%22${userId}%22`,
+      `https://apis.roblox.com/cloud/v2/groups/${process.env.ROBLOX_GROUP_ID}/memberships?pageSize=10`,
       {
         headers: {
           "x-api-key": process.env.ROBLOX_API_KEY
@@ -20,15 +10,12 @@ export default async function handler(req, res) {
       }
     );
 
-
-    const text = await response.text();
-
+    const data = await response.json();
 
     return res.status(200).json({
       kod: response.status,
-      cevap: text
+      veri: data
     });
-
 
   } catch (err) {
 
@@ -37,5 +24,4 @@ export default async function handler(req, res) {
     });
 
   }
-
 }
