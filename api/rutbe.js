a93a34a5b32b308911699b6ada16c3b6e701b1da
gs42dmb92g-commits <gs42dmb92g@privaterelay.appleid.com>
2026-07-10 14:34:26 +0300

    Update rutbe.js

--- api/rutbe.js
+++ api/rutbe.js
@@ -1,5 +1,27 @@
 export default async function handler(req, res) {
-
+const ranks = {
+  "Yardımcı Grup Sahibi": 60,
+  "Ordu Yönetimi": 50,
+  "Geliştirici Ekibi": 40,
+  "ÖZEL MİSAFİR": 39,
+  "Yönetim Kurulu Başkanı": 29,
+  "Yönetim Kurulu": 28,
+  "Albay": 16,
+  "Yarbay": 15,
+  "Binbaşı": 14,
+  "Yüzbaşı": 13,
+  "Teğmen": 12,
+  "Asteğmen": 11,
+  "Astsubay Kıdemli BaşÇavuş": 10,
+  "Astsubay BaşÇavuş": 9,
+  "Astsubay Kıdemli Çavuş": 8,
+  "Astsubay Çavuş": 7,
+  "Uzman Çavuş": 6,
+  "Uzman Onbaşı": 5,
+  "Çavuş": 4,
+  "Onbaşı": 3,
+  "Acemi Er": 2
+};
   if (req.method !== "POST") {
     return res.status(405).json({
       hata: "Sadece POST kullanılabilir"
