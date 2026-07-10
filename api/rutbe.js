7d682f25c91d5ddf1a0074370494667a017034d5
gs42dmb92g-commits <gs42dmb92g@privaterelay.appleid.com>
2026-07-10 16:34:49 +0300

    Update rutbe.js

--- api/rutbe.js
+++ api/rutbe.js
@@ -1,5 +1,5 @@
-export default async function handler(req, res) {
-  return res.status(200).json({
-    test: "YENI KOD CALISIYOR"
+export default function handler(req, res) {
+  res.status(200).json({
+    test: "CALISIYOR"
   });
 }
